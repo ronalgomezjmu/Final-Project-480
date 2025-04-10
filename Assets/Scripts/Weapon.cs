f84c5d6920e7f224f50b80d4d3304c4f483e9c2a
Adamv27 <adamvinch@gmail.com>
2025-04-09 21:39:32 -0400

    Add fire rate and reloading

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Weapon : MonoBehaviour
 {
@@ -7,16 +8,67 @@ public class Weapon : MonoBehaviour
 
     public int ammoCount = 10;
     private int currentAmmoCount;
+
+    public float timeBetweenShots = 1f;
+    private float shotCooldownRemaining = 0;
+
+    public GameObject reloadingText;
+    public Slider reloadProgress;
+    public float reloadTime = 2f;
+    private float reloadCooldownRemaining = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentAmmoCount = ammoCount;
     }
 
+    void Update()
+    {
+        if (shotCooldownRemaining > 0)
+        {
+            shotCooldownRemaining -= Time.deltaTime;
+        }
+
+        if (reloadCooldownRemaining > 0)
+        {
+            reloadCooldownRemaining -= Time.deltaTime;
+            reloadProgress.value = (reloadTime - reloadCooldownRemaining) / reloadTime;
+            if (reloadCooldownRemaining <= 0)
+            {
+                endReload();
+            }
+        }
+
+    }
+
+    private void startReload()
+    {
+        reloadCooldownRemaining = reloadTime;
+        reloadingText.SetActive(true);
+        reloadProgress.gameObject.SetActive(true);
+    }
+
+    public void endReload()
+    {
+        reloadingText.SetActive(false);
+        reloadProgress.gameObject.SetActive(false);
+        currentAmmoCount = ammoCount;
+    }
+
     public void shoot()
     {
-        GameObject bullet = Instantiate(bulletTempalte, firePoint.position, firePoint.rotation);
-        Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
-        rigidbody.AddForce(firePoint.up * 5000f);
+        if (currentAmmoCount > 0 && shotCooldownRemaining <= 0)
+        {
+            GameObject bullet = Instantiate(bulletTempalte, firePoint.position, firePoint.rotation);
+            Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
+            rigidbody.AddForce(firePoint.up * 5000f);
+            shotCooldownRemaining = timeBetweenShots;
+            currentAmmoCount -= 1;
+
+            if (currentAmmoCount == 0)
+            {
+                startReload();
+            }
+        }
     }
 }
