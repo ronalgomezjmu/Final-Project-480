3b9a9250a4941f5c4d768e021b1d4bd63c523920
ronalgomez <gomezra@dukes.jmu.edu>
2025-05-05 21:21:39 -0400

    the zombies can go over barriers now

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,15 +1,13 @@
 using TMPro;
 using UnityEngine;
-using System;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
-    public int score = 0;
-    public TextMeshProUGUI scoreText; // Drag in inspector
 
-    // Add event for other systems to listen to score changes
-    public event Action<int> OnScoreChanged;
+    public int score = 0;
+    public int scoreToUnlock = 100;
+    public TextMeshProUGUI scoreText; // Drag in inspector
 
     void Awake()
     {
@@ -20,18 +18,23 @@ public class ScoreManager : MonoBehaviour
     public void AddPoints(int points)
     {
         score += points;
-
-        // Update UI
         if (scoreText != null)
             scoreText.text = "Score: " + score;
 
-        // Trigger the event so other systems can respond
-        OnScoreChanged?.Invoke(score);
+        if (score >= scoreToUnlock)
+        {
+            UnlockNextArea();
+        }
     }
 
-    // Get current score
-    public int GetScore()
+    void UnlockNextArea()
     {
-        return score;
+        GameObject[] barriers = GameObject.FindGameObjectsWithTag("Barrier");
+        foreach (GameObject barrier in barriers)
+        {
+            Destroy(barrier);
+        }
+
+        Debug.Log("Next area unlocked!");
     }
 }
